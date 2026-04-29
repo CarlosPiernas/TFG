14e72b568e0389b58ea3bf8b6ff1856a5578a511
Driss <drissbz10@gmail.com>
2026-04-29 14:02:39 +0200

    fix: home ajustado y a falta de revisión

--- config.py
+++ config.py
@@ -16,6 +16,27 @@ GRIS             = hex_to_kivy('#808080')
 COLOR_CAMPAÑA    = hex_to_kivy('#009aa6')
 COLOR_VIDA       = hex_to_kivy('#00cc33')
 
+# Moneda e iconos de navegación
+ICONO_MONEDA     = 'assets/logos/Moneda.png'
+ICONO_GACHA      = 'assets/logos/IconoGacha.png'
+ICONO_INVENTARIO = 'assets/logos/IconoInventario.png'
+ICONO_ATK      = 'assets/logos/Ataque.png'
+ICONO_DEF      = 'assets/logos/Defensa.png'
+ICONO_DES      = 'assets/logos/Destreza.png'
+ICONO_MAG      = 'assets/logos/Magia.png'
+ICONO_POCION   = 'assets/logos/potions.png'
+PAPIRO_ANOMALIA  = 'assets/fondos/papiroAnomalo.png'
+PAPIRO_GUARDIAN  = 'assets/fondos/papiroGuardian.png'
+ICONO_CAMPAÑA    = 'assets/logos/IconoCampaña.png'
+FONDO_HOME       = 'assets/fondos/FondoPantallaHome.jpg'
+
+# Fondos bloque runas
+FONDO_RUNA_ANOMALIA  = 'assets/fondos/fondoRunaAnoma.jpg'
+FONDO_RUNA_GUARDIAN  = 'assets/fondos/fondoRunaGuard.jpg'
+
+# Placeholder para assets que faltan
+PLACEHOLDER = 'assets/test.png'
+
 # Logos
 LOGO_ANOMALIA = 'assets/logos/Logo_Anomalias.png'
 LOGO_GUARDIAN = 'assets/logos/Logo_Guardianes.png'
